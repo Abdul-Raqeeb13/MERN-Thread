1c42a412d72ec3cd779620f1eae6821536bc3e22
Abdul-Raqeeb13 <abdulraqeeb97568@gmail.com>
2025-05-01 00:36:55 +0500

    Middleware and Follow Unfollow Functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,7 +80,25 @@ const followUnFollowUser = async (req, res) => {
    try {
       const { id } = req.params;
       const userToModify = await User.findById(id)
-      const currentUser = await User.findById(req.user._id)
+      const currentUser = await User.findById(req.user._id) // login user
+
+
+      if(id === req.user._id) return res.status(400).json
+      ({message : "You cannot follow/unfollow yourself"})
+   
+      if(!userToModify || !currentUser) return res.status(400).json({message : "User not found"})
+
+      const isFollowing = currentUser.following.includes(id)
+
+      if (isFollowing) {
+         await User.findByIdAndUpdate(req.user._id, {$pull : {following : id }})
+         await User.findByIdAndUpdate(id, {$pull : {followers : req.user._id }})
+      }
+      else{
+         await User.findByIdAndUpdate(req.user._id, {$push : {following : id }})
+         await User.findByIdAndUpdate(id, {$push : {followers : req.user._id }})
+      }
+
    } catch (error) {
       res.status(500).json({ message: error.message })
       console.log("Error in logoutUser : ", error.message);
