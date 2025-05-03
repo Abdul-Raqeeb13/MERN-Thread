973159115ae14c249da308d38247d0b639f6c70f
Abdul-Raqeeb13 <abdulraqeeb97568@gmail.com>
2025-05-04 00:42:06 +0500

    Delete like comment and Feed Post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,7 +1,6 @@
 import User from "../models/userModel.js";
 import Post from "../models/postModel.js";
 
-
 const createPost = async (req, res) => {
     try {
         const { postedBy, text, img } = req.body;
@@ -31,4 +30,122 @@ const createPost = async (req, res) => {
     }
 }
 
-export { createPost }
+const getPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id.toString())
+
+        if (!post) {
+            return res.status(404).json({ message: "Post Not Found" })
+        }
+
+        res.status(200).json({ message: "Post Found", post })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+        console.log("Error in getPost  : ", error.message);
+    }
+}
+
+const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if (!post) {
+            return res.status(404).json({ message: "Post Not Found" })
+        }
+
+        if (post.postedBy.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ message: "Unauthorized to delete post" })
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({ message: "Delete post success", post })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+        console.log("Error in deletepost  : ", error.message);
+    }
+}
+
+const likeUnlikePost = async (req, res) => {
+    try {
+        const { id: postId } = req.params;
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId)
+        if (!post) {
+            return res.status(404).json({ message: "Post Not found" })
+        }
+
+        const userLikedPost = post.likes.includes(userId)
+        if (userLikedPost) {
+            // unlike post
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+            res.status(200).json({ message: "Post unliked successfully" });
+        } else {
+            // like post
+            post.likes.push(userId);
+            await post.save();
+            res.status(200).json({ message: "Post liked successfully" });
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+        console.log("Error in like and unlike post  : ", error.message);
+    }
+}
+
+const replyToPost = async (req, res) => {
+    try {
+
+        const { text } = req.body;
+        const { id: postId } = req.params;
+        const userId = req.user._id;
+        const userProfilePic = req.user.profilePic;
+        const username = req.user.username;
+
+        if (!text) {
+            res.status(400).json({ message: "Text field is required" })
+        }
+
+        const post = await Post.findById(postId)
+
+        if (!post) {
+            return res.status(404).json({ message: "Post Not found" })
+        }
+
+        const reply = { userId, text, userProfilePic, username };
+
+        post.replies.push(reply)
+        await post.save()
+
+        res.status(200).json(post)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+        console.log("Error in reply to post  : ", error.message);
+    }
+}
+
+const getFeedPost = async (req, res) => {
+	try {
+		const userId = req.user._id;
+		const user = await User.findById(userId);
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+        console.log(user);
+        
+		const following = user.following;
+        console.log(following);
+        
+		const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: -1 });
+
+		res.status(200).json(feedPosts);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
+export { createPost, getPost, deletePost, likeUnlikePost, replyToPost, getFeedPost }
